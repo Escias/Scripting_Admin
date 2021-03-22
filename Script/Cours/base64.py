2597da095ea611faf56563981fe8dc4c6ae0d9e4
Escias <ljimmy98@hotmail.fr>
2021-03-22 12:42:19 +0100

    1 to 8

--- Script/Cours/base64.py
+++ Script/Cours/base64.py
@@ -2,6 +2,7 @@ def string_to_list(str):
     li = []
     li[:0] = str
     print(li)
+    to_ascii(li)
     return li
 
 
@@ -10,15 +11,18 @@ def to_ascii(cha):
     for c in cha:
         li.append(ord(c))
     print(li)
+    to_binary(li)
     return li
 
 
 def to_binary(cha):
     li = []
     for c in cha:
-        li.append(bin(c))
+        li.append(bin(c).replace('b', ''))
     print(li)
-    return c
+    # frame_list(li)
+    list_to_string(li)
+    return li
 
 
 def frame_list(cha):
@@ -26,11 +30,13 @@ def frame_list(cha):
     for c in cha:
         li.append('0'+c)
     print(li)
+    list_to_string(li)
     return li
 
 
 def list_to_string(cha):
     print(''.join(cha))
+    bloc(''.join(cha), 6)
     return ''.join(cha)
 
 
@@ -38,6 +44,8 @@ def bloc(cha, n):
     li = []
     for i in range(0, len(cha), n):
         li.append(cha[i:i+n])
+    print(li)
+    last_bloc(li)
     return li
 
 
@@ -47,7 +55,25 @@ def last_bloc(cha):
         while len(c) < 6:
             c = c + '0'
         li.append(c)
+    print(li)
+    to_decimal(li)
     return li
 
+
+def to_decimal(cha):
+    li = []
+    for c in cha:
+        li.append(int(c, 2))
+    print(li)
+    to_base64(li)
+    return li
+
+
+def to_base64(cha):
+    li = []
+    for c in cha:
+        li.append(chr(c))
+    print(li)
+    return li
 
 
--- Script/main.py
+++ Script/main.py
@@ -1,6 +1,9 @@
-def main():
-    print('Hello World')
+from Script.Cours.base64 import string_to_list
+
+
+def main(li):
+    string_to_list(li)
 
 
 if __name__ == '__main__':
-    main()
+    main(li='ABCDE')
