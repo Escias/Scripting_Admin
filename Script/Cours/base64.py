65bbb5754cf38f29c64f638b195bb868d5fb27a7
Escias <ljimmy98@hotmail.fr>
2021-03-22 14:47:12 +0100

    convert to base64

--- Script/Cours/base64.py
+++ Script/Cours/base64.py
@@ -1,79 +1,124 @@
+import binascii
+
+
 def string_to_list(str):
+    '''
+    Convert string to list
+    :param str:
+    :return:
+    '''
     li = []
     li[:0] = str
     print(li)
-    to_ascii(li)
     return li
 
 
-def to_ascii(cha):
+def to_ascii(li_str):
+    '''
+    Convert each element of string to ASCII
+    :param li_str:
+    :return:
+    '''
     li = []
-    for c in cha:
-        li.append(ord(c))
-    print(li)
-    to_binary(li)
-    return li
-
-
-def to_binary(cha):
-    li = []
-    for c in cha:
-        li.append(bin(c).replace('b', ''))
-    print(li)
-    # frame_list(li)
-    list_to_string(li)
-    return li
-
-
-def frame_list(cha):
-    li = []
-    for c in cha:
-        li.append('0'+c)
-    print(li)
-    list_to_string(li)
-    return li
-
-
-def list_to_string(cha):
-    print(''.join(cha))
-    bloc(''.join(cha), 6)
-    return ''.join(cha)
-
-
-def bloc(cha, n):
-    li = []
-    for i in range(0, len(cha), n):
-        li.append(cha[i:i+n])
-    print(li)
-    last_bloc(li)
-    return li
-
-
-def last_bloc(cha):
-    li = []
-    for c in cha:
-        while len(c) < 6:
-            c = c + '0'
-        li.append(c)
-    print(li)
-    to_decimal(li)
-    return li
-
-
-def to_decimal(cha):
-    li = []
-    for c in cha:
-        li.append(int(c, 2))
-    print(li)
-    to_base64(li)
-    return li
-
-
-def to_base64(cha):
-    li = []
-    for c in cha:
-        li.append(chr(c))
+    for elem in li_str:
+        li.append(ord(elem))
     print(li)
     return li
 
 
+def to_binary(li_ascii):
+    '''
+    Convert each element (ascii) of list to binary
+    :param li_ascii:
+    :return:
+    '''
+    li = []
+    for elem in li_ascii:
+        li.append(bin(elem)[2:])
+    print(li)
+    return li
+
+
+def frame_list(li_binary):
+    '''
+    Crop each element of list to 8 position
+    :param li_binary:
+    :return:
+    '''
+    li = []
+    for elem in li_binary:
+        li.append(elem.zfill(8))
+    print(li)
+    return li
+
+
+def list_to_string(li):
+    '''
+    Convert list to String
+    :param li:
+    :return:
+    '''
+    print(''.join(li))
+    return ''.join(li)
+
+
+def bloc(srt, n):
+    '''
+    Cut the string into list of element of length n
+    :param srt:
+    :param n:
+    :return:
+    '''
+    li = []
+    for i in range(0, len(srt), n):
+        li.append(srt[i:i + n])
+    print(li)
+    return li
+
+
+def last_bloc(li_bloc):
+    '''
+    Crop each element of list to 6 position
+    :param li_bloc:
+    :return:
+    '''
+    li = []
+    for elem in li_bloc:
+        while len(elem) < 6:
+            elem = elem.ljust(6, '0')
+        li.append(elem)
+    print(li)
+    return li
+
+
+def to_decimal(li_binary):
+    '''
+    convert each element (binary) of list to decimal
+    :param li_binary:
+    :return:
+    '''
+    li = []
+    for elem in li_binary:
+        li.append(int(elem, 2))
+    print(li)
+    return li
+
+
+def to_base64(li_decimal):
+    '''
+    Convert decimal to base64 character
+    :param li_decimal:
+    :return:
+    '''
+    li = []
+    for elem in li_decimal:
+        li.append(chr(elem+65))
+    print(li)
+    return li
+
+
+def multiple_4(str64):
+    while len(str64) % 4 != 0:
+        str64 += '='
+    print(str64)
+    return str64
--- Script/main.py
+++ Script/main.py
@@ -1,4 +1,5 @@
-from Script.Cours.base64 import string_to_list
+from Script.Cours.base64 import string_to_list, to_ascii, to_binary, frame_list, list_to_string, bloc, last_bloc, \
+    to_decimal, to_base64, multiple_4
 
 
 def main(li):
@@ -6,4 +7,14 @@ def main(li):
 
 
 if __name__ == '__main__':
-    main(li='ABCDE')
+    to_list = string_to_list('ABCDE')
+    list_ascii = to_ascii(to_list)
+    list_binary = to_binary(list_ascii)
+    frame = frame_list(list_binary)
+    to_string = list_to_string(frame)
+    bloc = bloc(to_string, 6)
+    lbloc = last_bloc(bloc)
+    list_decimal = to_decimal(lbloc)
+    list_base64 = to_base64(list_decimal)
+    str_base64 = list_to_string(list_base64)
+    multiple_4(str_base64)
