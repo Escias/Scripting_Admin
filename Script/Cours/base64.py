aa5d5897a52bb2c8d9ce46369d06f762301f394b
Escias <ljimmy98@hotmail.fr>
2021-03-22 14:57:26 +0100

    add docs

--- Script/Cours/base64.py
+++ Script/Cours/base64.py
@@ -4,8 +4,8 @@ import binascii
 def string_to_list(str):
     '''
     Convert string to list
-    :param str:
-    :return:
+    :param str: string
+    :return: list
     '''
     li = []
     li[:0] = str
@@ -15,9 +15,9 @@ def string_to_list(str):
 
 def to_ascii(li_str):
     '''
-    Convert each element of string to ASCII
-    :param li_str:
-    :return:
+    Convert each element of list to ASCII
+    :param li_str: list of string
+    :return: list of ASCII
     '''
     li = []
     for elem in li_str:
@@ -29,8 +29,8 @@ def to_ascii(li_str):
 def to_binary(li_ascii):
     '''
     Convert each element (ascii) of list to binary
-    :param li_ascii:
-    :return:
+    :param li_ascii: list of ascii
+    :return: list of binary
     '''
     li = []
     for elem in li_ascii:
@@ -42,8 +42,8 @@ def to_binary(li_ascii):
 def frame_list(li_binary):
     '''
     Crop each element of list to 8 position
-    :param li_binary:
-    :return:
+    :param li_binary: list of binary
+    :return: list of rearranged binary element
     '''
     li = []
     for elem in li_binary:
@@ -55,8 +55,8 @@ def frame_list(li_binary):
 def list_to_string(li):
     '''
     Convert list to String
-    :param li:
-    :return:
+    :param li: list
+    :return: string based on the list
     '''
     print(''.join(li))
     return ''.join(li)
@@ -65,9 +65,9 @@ def list_to_string(li):
 def bloc(srt, n):
     '''
     Cut the string into list of element of length n
-    :param srt:
-    :param n:
-    :return:
+    :param srt: string
+    :param n: length of each element to add to the list
+    :return: list of rearranged string binary
     '''
     li = []
     for i in range(0, len(srt), n):
@@ -79,8 +79,8 @@ def bloc(srt, n):
 def last_bloc(li_bloc):
     '''
     Crop each element of list to 6 position
-    :param li_bloc:
-    :return:
+    :param li_bloc: list of rearranged string binary
+    :return: list of rearranged string binary
     '''
     li = []
     for elem in li_bloc:
@@ -94,8 +94,8 @@ def last_bloc(li_bloc):
 def to_decimal(li_binary):
     '''
     convert each element (binary) of list to decimal
-    :param li_binary:
-    :return:
+    :param li_binary: list of binary
+    :return: list of decimal
     '''
     li = []
     for elem in li_binary:
@@ -107,17 +107,23 @@ def to_decimal(li_binary):
 def to_base64(li_decimal):
     '''
     Convert decimal to base64 character
-    :param li_decimal:
-    :return:
+    :param li_decimal: list of decimal
+    :return: list of element encoded in base 64
     '''
     li = []
     for elem in li_decimal:
+        # letter begin at 65 in ASCII list
         li.append(chr(elem+65))
     print(li)
     return li
 
 
 def multiple_4(str64):
+    '''
+    change string to multiple of 4
+    :param str64: string
+    :return: string of length multiple of 4
+    '''
     while len(str64) % 4 != 0:
         str64 += '='
     print(str64)
