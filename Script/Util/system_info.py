ab39778c1edc5c146fdba16f6ab38c871641dafe
Escias <ljimmy98@hotmail.fr>
2021-03-23 16:44:20 +0100

    call system info

--- Script/Util/system_info.py
+++ Script/Util/system_info.py
@@ -1,4 +1,5 @@
 import psutil
+import datetime
 
 
 def get_cpu_info():
@@ -10,3 +11,38 @@ def get_cpu_info():
            'cpu_count': psutil.cpu_count(),
            'cpu_frequency': psutil.cpu_freq()}
     return cpu
+
+
+def get_ram_info():
+    '''
+
+    :return:
+    '''
+    ram = {'ram_usage': psutil.virtual_memory()}
+    return ram
+
+
+def get_disk_info():
+    '''
+
+    :return:
+    '''
+    disk = {'disk_partitions': psutil.disk_partitions(),
+            'disk_usage': psutil.disk_usage('/')}
+    return disk
+
+
+def get_network_info():
+    network = {'network_io_counter': psutil.net_io_counters(pernic=False, nowrap=True),
+               'network_address': psutil.net_if_addrs()}
+    return network
+
+
+def get_sensors_info():
+    sensor = {'sensor_temp': psutil.sensors_battery()}
+    return sensor
+
+
+def get_system_info():
+    system = {'system_boot_time': datetime.datetime.fromtimestamp(psutil.boot_time()).strftime("%Y-%m-%d %H:%M:%S")}
+    return system
--- Script/main.py
+++ Script/main.py
@@ -1,14 +1,30 @@
-from Script.Util.system_info import get_cpu_info
+from Script.Util.system_info import get_cpu_info, get_ram_info, get_disk_info, get_sensors_info, get_network_info, \
+    get_system_info
 
 
 def main():
     '''
-
-    :return:
+    Main function of application
+    :return: None
     '''
-    cpu = get_cpu_info()
-    for value in cpu:
-        print('{} : {}'.format(value, cpu[value]))
+    cpu_info = get_cpu_info()
+    ram_info = get_ram_info()
+    disk_info = get_disk_info()
+    network_info = get_network_info()
+    sensors_info = get_sensors_info()
+    system_info = get_system_info()
+    for value in cpu_info:
+        print('{} : {}'.format(value, cpu_info[value]))
+    for value in ram_info:
+        print('{} : {}'.format(value, ram_info[value]))
+    for value in disk_info:
+        print('{} : {}'.format(value, disk_info[value]))
+    for value in network_info:
+        print('{} : {}'.format(value, network_info[value]))
+    for value in sensors_info:
+        print('{} : {}'.format(value, sensors_info[value]))
+    for value in system_info:
+        print('{} : {}'.format(value, system_info[value]))
 
 
 if __name__ == '__main__':
