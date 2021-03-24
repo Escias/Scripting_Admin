c1762625815f1dc87b28c29156b2cf50e57dc2ee
Full Name <anis.bastide@gmail.com>
2021-03-24 17:23:25 +0100

    added information on db

--- Script/Util/system_info.py
+++ Script/Util/system_info.py
@@ -1,18 +1,10 @@
 import psutil
 import datetime
+import time
 import socket
 from getmac import get_mac_address as gma
 
 from Script.Util.database import send_info_to_DB
-
-
-def get_ip_client():
-    '''
-    Get mac address
-    :return: mac address
-    '''
-    ip_client = gma()
-    return ip_client
 
 
 def get_cpu_info():
@@ -21,8 +13,9 @@ def get_cpu_info():
     :return: dictionary with cpu informations
     '''
     cpu = {'cpu_percent': psutil.cpu_percent(interval=1),
-           'cpu_count': psutil.cpu_count(),
            'cpu_frequency': psutil.cpu_freq().current}
+    send_info_to_DB("cpu", "mac_anis", "cpu_utilisation", cpu["cpu_percent"])
+    send_info_to_DB("cpu", "mac_anis", "cpu_frequency", cpu["cpu_frequency"])
     return cpu
 
 
@@ -32,6 +25,7 @@ def get_ram_info():
     :return: dictionary with ram informations
     '''
     ram = {'ram_usage': psutil.virtual_memory().percent}
+    send_info_to_DB("ram", "mac_anis", "ram_utilisation", ram["ram_usage"])
     return ram
 
 
@@ -41,6 +35,7 @@ def get_disk_info():
     :return: dictionary with disk informations
     '''
     disk = {'disk_usage': psutil.disk_usage('/').percent}
+    send_info_to_DB("disk", "mac_anis", "ram_utilisation", disk["disk_usage"])
     return disk
 
 
@@ -52,22 +47,21 @@ def get_network_info():
     network = {'network_bytes_sent': psutil.net_io_counters(pernic=False, nowrap=True).bytes_sent,
                'network_bytes_receive': psutil.net_io_counters(pernic=False, nowrap=True).bytes_recv,
                'network_packets_sent': psutil.net_io_counters(pernic=False, nowrap=True).packets_sent,
-               'network_packets_receive': psutil.net_io_counters(pernic=False, nowrap=True).packets_recv}
-
+               'network_packets_receive': psutil.net_io_counters(pernic=False, nowrap=True).packets_recv,
+               'errin': psutil.net_io_counters(pernic=False, nowrap=True).errin,
+               'errout': psutil.net_io_counters(pernic=False, nowrap=True).errout,
+               'dropin': psutil.net_io_counters(pernic=False, nowrap=True).dropin,
+               'dropout': psutil.net_io_counters(pernic=False, nowrap=True).dropout,
+               }
+    send_info_to_DB("network", "mac_anis", "bytes_sent", network["network_bytes_sent"])
+    send_info_to_DB("network", "mac_anis", "bytes_received", network["network_bytes_receive"])
+    send_info_to_DB("network", "mac_anis", "packets_sent", network["network_packets_sent"])
+    send_info_to_DB("network", "mac_anis", "packets_received", network["network_packets_receive"])
+    send_info_to_DB("network", "mac_anis", "errin", network["errin"])
+    send_info_to_DB("network", "mac_anis", "errout", network["errout"])
+    send_info_to_DB("network", "mac_anis", "dropin", network["dropin"])
+    send_info_to_DB("network", "mac_anis", "dropout", network["dropout"])
     return network
-
-
-def get_sensors_info():
-    '''
-    Get sensors information
-    :return: dictionary with sensors informations
-    '''
-    try:
-        sensor = ""
-        sensor = {'sensor_battery': psutil.sensors_battery().percent}
-    except (AttributeError, TypeError):
-        print('[WARN] User has no battery !')
-    return sensor
 
 
 def get_system_info():
@@ -75,5 +69,9 @@ def get_system_info():
     Get system information
     :return: dictionary with system informations
     '''
-    system = {'system_boot_time': datetime.datetime.fromtimestamp(psutil.boot_time()).strftime("%Y-%m-%d %H:%M:%S")}
+    s = str(datetime.datetime.now())[:-7]
+    d = datetime.datetime.strptime(s, "%Y-%m-%d %H:%M:%S")
+    uptime=str(datetime.timedelta(seconds=time.mktime(d.timetuple())-psutil.boot_time()))
+    system = {'system_uptime': uptime}
+    send_info_to_DB("device", "mac_anis", "uptime", system["system_uptime"])
     return system
--- Script/main.py
+++ Script/main.py
@@ -1,5 +1,5 @@
-from Script.Util.system_info import get_cpu_info, get_ram_info, get_disk_info, get_sensors_info, get_network_info, \
-    get_system_info, get_ip_client
+from Script.Util.system_info import get_cpu_info, get_ram_info, get_disk_info, get_network_info, \
+    get_system_info
 
 
 def main():
@@ -7,14 +7,11 @@ def main():
     Main function of application
     :return: None
     '''
-    ip_client = get_ip_client()
     cpu_info = get_cpu_info()
     ram_info = get_ram_info()
     disk_info = get_disk_info()
     network_info = get_network_info()
-    sensors_info = get_sensors_info()
     system_info = get_system_info()
-    print(ip_client)
     for value in cpu_info:
         print('{} : {}'.format(value, cpu_info[value]))
     for value in ram_info:
@@ -23,8 +20,6 @@ def main():
         print('{} : {}'.format(value, disk_info[value]))
     for value in network_info:
         print('{} : {}'.format(value, network_info[value]))
-    for value in sensors_info:
-        print('{} : {}'.format(value, sensors_info[value]))
     for value in system_info:
         print('{} : {}'.format(value, system_info[value]))
 
